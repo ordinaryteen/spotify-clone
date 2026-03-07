26ac0deeb79d55da807dea4c77c5a7b06fdf52a8
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-03-07 09:24:54 +0700

    feat(ui): setup dark and light mode

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/theme/app_colors.dart';
 
+class AppTheme {
+  static final lightTheme = ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      brightness: Brightness.light,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(30)) // RoundedRectangleBorder
+              )) // ElevatedButtonThemeData
+      ); // ThemeData
+
+  static final darkTheme = ThemeData(
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      brightness: Brightness.dark,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(30)) // RoundedRectangleBorder
+              )) // ElevatedButtonThemeData
+      );
+}
