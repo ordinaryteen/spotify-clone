8a764d55c12ef29e5345f05a9aaa1e8405171660
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-03-07 16:16:16 +0700

    refactor(di): implement modular DI

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,15 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-// Import semua layer yang ada
-import 'package:spotify_clone/src/features/auth/data/datasources/auth_remote_data_source_impl.dart';
-import 'package:spotify_clone/src/features/auth/data/repositories/auth_repo_impl.dart';
-import 'package:spotify_clone/src/features/auth/domain/repositories/auth_repo.dart';
-import 'package:spotify_clone/src/features/auth/domain/usecases/get_user_usecase.dart';
-import 'package:spotify_clone/src/features/auth/domain/usecases/sign_in_usecase.dart';
-import 'package:spotify_clone/src/features/auth/domain/usecases/sign_up_usecase.dart';
-import 'package:spotify_clone/src/features/auth/domain/usecases/sign_out_usecase.dart';
-import 'package:spotify_clone/src/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:spotify_clone/core/di/auth_di.dart';
 
 final sl = GetIt.instance;
 
@@ -17,33 +9,15 @@ Future<void> initializeDependencies() async {
   // -------------------------
   // CORE / EXTERNAL
   // -------------------------
+  // Daftarin Supabase Client
   sl.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
 
+  // TODO:(Nanti kalau ada Isar, SharedPreferences, atau Dio, daftarin di sini)
+
   // -------------------------
-  // FEATURES: AUTH
+  // FEATURES INJECTIONS
   // -------------------------
+  initAuthDI(sl);
 
-  // 1. Data Sources
-  sl.registerLazySingleton<AuthRemoteDataSource>(
-    () => AuthRemoteDataSourceImpl(sl()),
-  );
-
-  // 2. Repositories
-  sl.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(sl()),
-  );
-
-  // 3. Use Cases
-  sl.registerLazySingleton(() => SignUpUseCase(sl()));
-  sl.registerLazySingleton(() => SignInUseCase(sl()));
-  sl.registerLazySingleton(() => GetCurrentUserUseCase(sl()));
-  sl.registerLazySingleton(() => SignOutUseCase(sl()));
-
-  // 4. BLoC
-  sl.registerFactory(() => AuthBloc(
-        sl(),
-        sl(),
-        sl(),
-        sl(),
-      ));
+  // initSongsDI(sl);
 }
